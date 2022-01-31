0955fd9c7fbf734a6cbcf0cbdd173f6f11b7c9d5
atharv joshi <atharv.joshi7869@gmail.com>
2022-01-31 21:10:44 +0530

    add about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def hello
+    def home
         render html: "Hello world!"
     end
 
--- db/migrate/20220131125034_create_pages.rb
+++
@@ -1,8 +0,0 @@
-class CreatePages < ActiveRecord::Migration[7.0]
-  def change
-    create_table :pages do |t|
-
-      t.timestamps
-    end
-  end
-end
